2c929f6240f2c969425b4d4db4605fa795169186
teenybopper <mayank.jo@outlook.com>
2025-09-27 00:46:45 +0530

    changed the model and the yaml file

--- utils/rag/get_models.py
+++ utils/rag/get_models.py
@@ -1,8 +1,13 @@
 from llama_index.llms.ollama import Ollama
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.openai import OpenAI
 
 from ..config import CONFIG
+import os 
+from dotenv import load_dotenv
+load_dotenv("/home/mayank/Documents/dollarstore/test/.env")
 
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
 TEMPERATURE = CONFIG["TEMPERATURE"]
 TIMEOUT = CONFIG["TIMEOUT"]
@@ -13,8 +18,10 @@ def get_embedding_model(embedding_model: str):
     return embed_model
 
 def get_llm_model(llm_model: str, temp: float=TEMPERATURE, timeout: int=TIMEOUT):
-    llm = Ollama(
-        model=llm_model, 
+    llm = OpenAI(
+        model_name=llm_model, 
         temperature=temp, 
-        request_timeout=timeout)
+        openai_api_key=openai_api_key,  # Pass the OpenAI API Key
+        request_timeout=timeout
+    )
     return llm
